ed266baed0c9f04e6b018ae6280dc3911c2dd288
Harish Brahme <harishbrahme@gmail.com>
2020-10-11 23:43:34 +0530

    B&G Cakes

---
+++ app/src/main/java/com/example/bg/RegisterActivity.java
@@ -0,0 +1,47 @@
+package com.example.bg;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.FrameLayout;
+
+public class RegisterActivity extends AppCompatActivity {
+    private FrameLayout frameLayout;
+    public static boolean onResetPasswordFragment =false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        frameLayout = findViewById(R.id.register_framelayout);
+        setDefaultFragment(new SignInFragment());
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==KeyEvent.KEYCODE_BACK){
+            onResetPasswordFragment=false;
+            if (onResetPasswordFragment){
+                setFragment(new SignInFragment());
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private  void setDefaultFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(frameLayout.getId(),fragment);
+        fragmentTransaction.commit();
+    }
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction =getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_left,R.anim.slide0ut_from_right);
+        fragmentTransaction.replace(frameLayout.getId(),fragment);
+        fragmentTransaction.commit();
+    }
+
+}
